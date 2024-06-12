574a53bfbe36f5a2c01a2c564ba1cae907e8a29a
ithirajdev <ithiraj.dev@gmail.com>
2024-06-12 03:49:38 -0700

    ADD: TRANSACTION RESOLVER ADDED

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,68 @@
+import Transaction from "../models/transaction.model";
+
 const transactionResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    transactions: async (_, _, context) => {
+      try {
+        if(!context.getUser()) throw new Error("Unauthorized")
+        
+        const userId = context.getUser()._id;
+
+        const transactions = await Transaction.find({ userId });
+
+        return transactions;
+      } catch (error) {
+        console.error("Error getting transactions: ", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    },
+    transaction: async (_, args) => {
+      try {
+        const transaction = await Transaction.findById(args.transactionId);
+        return transaction;
+      } catch (error) {
+        console.error("Error getting transaction: ", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    }
+  },
+  Mutation: {
+    createTransaction: async (_, args, context) => {
+      try {
+        const newTransaction = new Transaction({
+          ...args.input,
+          userId: context.getUser()._id,
+        })
+
+        await newTransaction.save();
+
+        return newTransaction;
+      } catch (error) {
+        console.error("Error creating transaction: ", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    },
+    updateTransaction: async (_, args) => {
+      try {
+        const updatedTransaction = Transaction.findByIdAndUpdate(args.input.transactionId, input, {new: true});
+
+        return updatedTransaction;
+      } catch (error) {
+        console.error("Error updating transaction: ", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    },
+    deleteTransaction: async (_, args) => {
+      try {
+        const deletedTransaction = Transaction.findByIdAndDelete(args.transactionId);
+
+        return deletedTransaction;
+      } catch (error) {
+        console.error("Error deleting transaction: ", error);
+        throw new Error(error.message || "Internal server error");
+      }
+    }
+  },
 };
 
 export default transactionResolver;
